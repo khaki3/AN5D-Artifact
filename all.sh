07d4603c5b77a662105591a6631a0fe8ae0e6acf
Hidden <47756807+khaki3@users.noreply.github.com>
2019-12-06 05:20:14 +0900

    Add all.sh

---
+++ all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+GPUNAME=$(nvidia-smi -L | grep -oP '[PV]100' | tr '[PV]' '[pv]')
+
+if [[ -z $GPUNAME ]]
+then
+	echo "Could not find Tesla P100 or V100"
+	exit 1
+fi
+
+cd compiled
+
+for regnum in 0 32 64 96; do
+    ./nvcc_compile_top5.sh $(nproc) $regnum
+    ./run.sh $GPUNAME
+    cat $GPUNAME/float_run.csv >> ../tuned.float.sh
+    cat $GPUNAME/double_run.csv >> ../tuned.double.sh
+done
+
+cd ..
+
+./run_sconf.sh > sconf.log
